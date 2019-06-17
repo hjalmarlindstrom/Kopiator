3342fdd21bdbfe40b5b480e997cbe4b499d748e5
hjalmarlindstrom <51903587+hjalmarlindstrom@users.noreply.github.com>
2019-06-17 09:16:53 +0200

    Add files via upload

---
+++ kopiator.py
@@ -0,0 +1,4 @@
+antal_gånger = int(input("Hur många gånger vill du kopiera? "))
+vad_man_vill_kopiera =str(input("Klistra in eller skriv det du vill kopiera här intill "))
+for i in range(antal_gånger):
+    print(vad_man_vill_kopiera)
